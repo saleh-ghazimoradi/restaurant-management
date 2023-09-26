dd2562cd4526ce7d0380be93aab695e6b53b4969
Saleh Ghazimoradi <74179371+saleh-ghazimoradi@users.noreply.github.com>
2023-09-26 12:30:53 +0330

    set the config and logger

---
+++ cmd/api/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *slog.Logger
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf("%:d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
+
+	err := srv.ListenAndServe()
+
+	logger.Error(err.Error())
+	os.Exit(1)
+}
