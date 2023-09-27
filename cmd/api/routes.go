f73a8f86ccf3ace60429ec050f916d5b22372fe0
Saleh Ghazimoradi <74179371+saleh-ghazimoradi@users.noreply.github.com>
2023-09-27 12:14:17 +0330

    complete routers by adding more to the project

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -27,6 +27,48 @@ func (app *application) routes() *httprouter.Router {
 
 	router.HandlerFunc(http.MethodPatch, "/v1/foods/:id", app.updateFoodHandler)
 
+	router.HandlerFunc(http.MethodGet, "/v1/invoices", app.invoicesHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/invoices/:invoice_id", app.invoiceHandler)
+
+	router.HandlerFunc(http.MethodPost, "/v1/invoices", app.createInvoiceHandler)
+
+	router.HandlerFunc(http.MethodPatch, "/v1/invoices/:invoice_id", app.updateInvoiceHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/menus", app.menusHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/menus/:menu_id", app.menuHandler)
+
+	router.HandlerFunc(http.MethodPost, "/v1/menus", app.createMenuHandler)
+
+	router.HandlerFunc(http.MethodPatch, "/v1/menu/:menu_id", app.updateMenuHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/orders", app.ordersHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/orders/:order_id", app.orderHandler)
+
+	router.HandlerFunc(http.MethodPost, "/v1/orders", app.createOrderHandler)
+
+	router.HandlerFunc(http.MethodPatch, "/v1/orders/:order_id", app.updateOrderHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/tables", app.tablesHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/tables/:table_id", app.tableHandler)
+
+	router.HandlerFunc(http.MethodPost, "/v1/tables", app.createTableHandler)
+
+	router.HandlerFunc(http.MethodPatch, "/v1/tables/:table_id", app.updateTableHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/orderItems", app.orderItemsHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/orderItems/:orderItem_id", app.orderItemHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/orderItems-order/:order_id", app.orderItemsByOrderHandler)
+
+	router.HandlerFunc(http.MethodPost, "/v1/orderItems", app.createOrderItemHandler)
+
+	router.HandlerFunc(http.MethodPatch, "/v1/orderItems/:orderItem_id", app.updateOrderItemHandler)
+
 	return router
 
 }
