f508859d66844acb3f0582f689d382d424193548
Saleh Ghazimoradi <74179371+saleh-ghazimoradi@users.noreply.github.com>
2023-09-28 14:24:03 +0330

    improve the url routers

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -33,45 +33,45 @@ func (app *application) routes() *httprouter.Router {
 
 	router.HandlerFunc(http.MethodGet, "/v1/invoices", app.invoicesHandler)
 
-	router.HandlerFunc(http.MethodGet, "/v1/invoices/:invoice_id", app.invoiceHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/invoices/:id", app.invoiceHandler)
 
 	router.HandlerFunc(http.MethodPost, "/v1/invoices", app.createInvoiceHandler)
 
-	router.HandlerFunc(http.MethodPatch, "/v1/invoices/:invoice_id", app.updateInvoiceHandler)
+	router.HandlerFunc(http.MethodPatch, "/v1/invoices/:id", app.updateInvoiceHandler)
 
 	router.HandlerFunc(http.MethodGet, "/v1/menus", app.menusHandler)
 
-	router.HandlerFunc(http.MethodGet, "/v1/menus/:menu_id", app.menuHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/menus/:id", app.menuHandler)
 
 	router.HandlerFunc(http.MethodPost, "/v1/menus", app.createMenuHandler)
 
-	router.HandlerFunc(http.MethodPatch, "/v1/menu/:menu_id", app.updateMenuHandler)
+	router.HandlerFunc(http.MethodPatch, "/v1/menu/:id", app.updateMenuHandler)
 
 	router.HandlerFunc(http.MethodGet, "/v1/orders", app.ordersHandler)
 
-	router.HandlerFunc(http.MethodGet, "/v1/orders/:order_id", app.orderHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/orders/:id", app.orderHandler)
 
 	router.HandlerFunc(http.MethodPost, "/v1/orders", app.createOrderHandler)
 
-	router.HandlerFunc(http.MethodPatch, "/v1/orders/:order_id", app.updateOrderHandler)
+	router.HandlerFunc(http.MethodPatch, "/v1/orders/:id", app.updateOrderHandler)
 
 	router.HandlerFunc(http.MethodGet, "/v1/tables", app.tablesHandler)
 
-	router.HandlerFunc(http.MethodGet, "/v1/tables/:table_id", app.tableHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/tables/:id", app.tableHandler)
 
 	router.HandlerFunc(http.MethodPost, "/v1/tables", app.createTableHandler)
 
-	router.HandlerFunc(http.MethodPatch, "/v1/tables/:table_id", app.updateTableHandler)
+	router.HandlerFunc(http.MethodPatch, "/v1/tables/:id", app.updateTableHandler)
 
 	router.HandlerFunc(http.MethodGet, "/v1/orderItems", app.orderItemsHandler)
 
-	router.HandlerFunc(http.MethodGet, "/v1/orderItems/:orderItem_id", app.orderItemHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/orderItems/:id", app.orderItemHandler)
 
-	router.HandlerFunc(http.MethodGet, "/v1/orderItems-order/:order_id", app.orderItemsByOrderHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/orderItems-order/:id", app.orderItemsByOrderHandler)
 
 	router.HandlerFunc(http.MethodPost, "/v1/orderItems", app.createOrderItemHandler)
 
-	router.HandlerFunc(http.MethodPatch, "/v1/orderItems/:orderItem_id", app.updateOrderItemHandler)
+	router.HandlerFunc(http.MethodPatch, "/v1/orderItems/:id", app.updateOrderItemHandler)
 
 	return router
 
